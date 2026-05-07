6dbe52e2ae37fd236bc34028e500232487248f0e
Isasola <98595918+Isasola@users.noreply.github.com>
2026-05-07 08:55:47 -0400

    fix: forzar nuevo hash de bundle para invalidar CDN Netlify

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+const VERSION = '2.1.0';
+console.log(`CVitae v${VERSION}`);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// v2.1
